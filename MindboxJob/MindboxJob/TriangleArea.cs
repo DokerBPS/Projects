36679b276e8a64ca2f2d1204ddf023466796e16f
Pavel Babii <pa6a_medved@bk.ru>
2015-11-06 15:02:31 +0200

    Add test project for Minbox
    Test for Mindbox Job

---
+++ MindboxJob/MindboxJob/TriangleArea.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MindboxJob
+{
+    public class TriangleArea
+    {
+        /// <summary>
+        /// Calculate tirangle area (for whole numbers)
+        /// </summary>
+        /// <param name="side1">Width first side</param>
+        /// <param name="side2">Width second side</param>
+        /// <returns>Triangle area</returns>
+        public int GetTriangleArea(int side1, int side2)
+        {
+            return CalcArea(side1, side2);
+        }
+
+        /// <summary>
+        /// Calculate tirangle area (for real numbers)
+        /// </summary>
+        /// <param name="side1">Width first side</param>
+        /// <param name="side2">Width second side</param>
+        /// <returns>Triangle area</returns>
+        public decimal GetTriangleArea(decimal side1, decimal side2)
+        {
+            return CalcArea(side1, side2);
+        }
+
+        private dynamic CalcArea(dynamic side1, dynamic side2)
+        {
+            if (side1 == 0 || side2 == 0)
+                return 0;
+
+            return (side1 * side2) / 2;
+        }
+    }
+}
---
+++ MindboxJob/UsingExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MindboxJob;
+
+namespace MindboxJob.UsingExample
+{
+    class UsingExample
+    {
+        public void CalcArea()
+        {
+            TriangleArea calcArea = new TriangleArea();
+            decimal sideDec1 = 25.6M, sideDec2 = 15.6M;
+            int sideInt1 = 3, sideInt2 = 4;
+
+            var resultDecimal = calcArea.GetTriangleArea(sideDec1, sideDec2);
+            var resultInt = calcArea.GetTriangleArea(sideInt1, sideInt2);
+
+            Console.WriteLine("Result decimal: " + resultDecimal);
+            Console.WriteLine("Result int: " + resultInt);
+            Console.ReadLine();
+        }
+    }
+}
